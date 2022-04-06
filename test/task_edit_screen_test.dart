f399daaf7f8f72920d277e94a9df8ebab476f26b
Adam17c <a.wodzislawski@interia.pl>
2022-04-07 01:38:03 +0200

    added Task tests

---
+++ test/task_edit_screen_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:todo_app_ver1/models/task.dart';
+
+void main() {
+  group("Task tests", () {
+    final emptyTask = Task(id: "1", userId: "1");
+    test("Task description default value", () {
+      expect(emptyTask.description, "");
+    });
+    test("Task title default value", () {
+      expect(emptyTask.title, "");
+    });
+    test("Task isDone default value", () {
+      expect(emptyTask.isDone, false);
+    });
+    test("Task title passed through constuctor", () {
+      const taskTitle = "someTtile";
+      final titledTask = Task(id: "2", userId: "1", title: taskTitle);
+      expect(titledTask.title, taskTitle);
+    });
+  });
+
+  //
+}
